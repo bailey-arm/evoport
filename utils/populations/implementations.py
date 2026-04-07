601d34e37ea76618c9ff3e5545c7209b06a9b279
Bailey Arm <bailey.arm.business@com>
2026-04-07 18:03:13 +0100

    feat: added mandatory projection operations

--- utils/populations/implementations.py
+++ utils/populations/implementations.py
@@ -7,23 +7,39 @@ class LongOnlyBC(Population):
         super().__init__(problem_dimension, population_size)
         self.budget = budget
         self.initialise()
-    
+
     def initialise(self):
-        w = np.random.random(size = (self.population_size, self.problem_dimension))
+        w = np.random.random(size=(self.population_size, self.problem_dimension))
         w /= w.sum(axis=1, keepdims=True)
         w *= self.budget
         self.initial_weights = w
+
+    def project(self, weights: np.ndarray) -> np.ndarray:
+        w = np.maximum(weights, 0)
+        sums = w.sum(axis=1, keepdims=True)
+        sums = np.where(sums == 0, 1, sums)
+        w = w / sums * self.budget
+        return w
+
 
 class LongOnlyBR(Population):
     def __init__(self, problem_dimension: int, population_size: int = DEFAULT_POPULATION_SIZE, budget_max: float = 1):
         super().__init__(problem_dimension, population_size)
         self.budget_max = budget_max
         self.initialise()
-    
+
     def initialise(self):
-        budgets = np.random.random(size = (self.population_size)) * self.budget_max
-        w = np.random.random(size = (self.population_size, self.problem_dimension))
+        budgets = np.random.random(size=(self.population_size,)) * self.budget_max
+        w = np.random.random(size=(self.population_size, self.problem_dimension))
         w /= w.sum(axis=1, keepdims=True)
         for dim in range(self.problem_dimension):
-            w[:,dim] = np.multiply(w[:,dim], budgets)
+            w[:, dim] = np.multiply(w[:, dim], budgets)
         self.initial_weights = w
+
+    def project(self, weights: np.ndarray) -> np.ndarray:
+        w = np.maximum(weights, 0)
+        sums = w.sum(axis=1, keepdims=True)
+        over = sums > self.budget_max
+        scale = np.where(over, self.budget_max / np.where(sums == 0, 1, sums), 1)
+        w = w * scale
+        return w
