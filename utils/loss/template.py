e4847548c4c9c27944c4cb71c81215907fd99145
Bailey Arm <bailey.arm.business@com>
2026-04-07 13:30:59 +0100

    feat: higher level list of losses

--- utils/loss/template.py
+++ utils/loss/template.py
@@ -9,4 +9,23 @@ class Loss(ABC):
     @abstractmethod
     def evaluate(self, weights: np.array, data_package: dict):
         pass
+
+    @abstractmethod
+    def data_needed(self) -> list:
+        pass
+
+class ListLoss:
+    def __init__(self, losses: list[Loss]) -> None:
+        self.losses = losses
+
+    def evaluate(self, weights: np.array, data_package: dict) -> dict:
+        loss_values = {}
+        for loss in self.losses:
+            loss_values[loss.name] = loss.evaluate(weights, data_package)
+        return loss_values  
     
+    def data_needed(self) -> list:
+        data_needed = []
+        for loss in self.losses:
+            data_needed += loss.data_needed()
+        return list(set(data_needed))
