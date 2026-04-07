67817c23a65c5d4bd4b48c691dee0341437bb19c
Bailey Arm <bailey.arm.business@com>
2026-04-07 16:33:50 +0100

    feat: added minimax metastructure capacity

--- utils/loss/template.py
+++ utils/loss/template.py
@@ -13,10 +13,15 @@ class Loss(ABC):
     @abstractmethod
     def data_needed(self) -> list:
         pass
+    
+    @abstractmethod
+    def to_minimise(self) -> bool:
+        pass 
 
 class ListLoss:
     def __init__(self, losses: list[Loss]) -> None:
         self.losses = losses
+        self.load_minimax_structure()
 
     def evaluate(self, weights: np.array, data_package: dict) -> dict:
         loss_values = {}
@@ -29,3 +34,10 @@ class ListLoss:
         for loss in self.losses:
             data_needed += loss.data_needed()
         return list(set(data_needed))
+    
+    def load_minimax_structure(self) -> None:
+        losses = self.losses
+        structure = []
+        for loss in losses:
+            structure.append(loss.to_minimise)
+        self.minimax_structure = structure
