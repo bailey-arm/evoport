491b4727a5d23984328b3fb0044a77a812ac8f48
Bailey Arm <bailey.arm.business@com>
2026-04-07 17:08:50 +0100

    feat: added random budget to population imnplementation

--- utils/populations/implementations.py
+++ utils/populations/implementations.py
@@ -13,3 +13,17 @@ class LongOnlyBC(Population):
         w /= w.sum(axis=1, keepdims=True)
         w *= self.budget
         self.initial_weights = w
+
+class LongOnlyBR(Population):
+    def __init__(self, problem_dimension: int, population_size: int = DEFAULT_POPULATION_SIZE, budget_max: float = 1):
+        super().__init__(problem_dimension, population_size)
+        self.budget_max = budget_max
+        self.initialise()
+    
+    def initialise(self):
+        budgets = np.random.random(size = (self.population_size)) * self.budget_max
+        w = np.random.random(size = (self.population_size, self.problem_dimension))
+        w /= w.sum(axis=1, keepdims=True)
+        for dim in range(self.problem_dimension):
+            w[:,dim] = np.multiply(w[:,dim], budgets)
+        self.initial_weights = w
